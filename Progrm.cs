7a677635b239882ddf7dea6b85625ec7b8a6e70f
85534 <ismaelferreiradasilva431@gmail.com>
2026-06-16 12:18:06 -0300

    Create Progrm.cs

---
+++ Progrm.cs
@@ -0,0 +1,17 @@
+iphone
+namespace DesafioPOO.Models
+{
+	// Classe implementada
+	public class Iphone : Smartphone
+	{
+		// Método Implementado
+		public Iphone(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+		{
+		}
+
+		public override void InstalarAplicativo(string nomeApp)
+		{
+			Console.WriteLine($"Instalado o aplicativo \"{nomeApp}\" no IPhone");
+		}
+	}
+}
